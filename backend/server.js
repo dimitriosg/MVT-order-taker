5cc58cc57d6d76e71ecf4156c87235e76cd4ffc6
Dimitris Zeus <63305437+dimitriosg@users.noreply.github.com>
2023-10-08 23:03:39 +0300

    Fix Heroku build issues

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,8 @@ const app = express();
 const corsOptions = {
   origin: [
     'http://localhost:3000',
-    'https://order-taker-front-8e7edf8fac75.herokuapp.com'
+    'https://order-taker-front-8e7edf8fac75.herokuapp.com',
+    'https://mvt-order-taker-027623a22a27.herokuapp.com/'
   ],  // Allow both your local and deployed frontends
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,

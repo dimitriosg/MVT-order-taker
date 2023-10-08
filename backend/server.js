51d7a0f59e0f670f5db27210e616842d7dce841e
Dimitrios Galanopoulos <63305437+dimitriosg@users.noreply.github.com>
2023-10-08 22:29:31 +0300

    Update server.js
    putting comments on corsOptions

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 const corsOptions = {
   origin: [
     'http://localhost:3000',
-    'https://order-taker-front-8e7edf8fac75.herokuapp.com'
+    'https://order-taker-front-8e7edf8fac75.herokuapp.com' // change if necessary
   ],  // Allow both your local and deployed frontends
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
